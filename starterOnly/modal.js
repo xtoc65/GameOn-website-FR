1b31ff59bf8f2603dc364e8dce3c3e57dfbda2e6
Flora Paul <flora.p3108@gmail.com>
2024-01-31 10:02:47 +0100

    femeture du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close"); //on va cherche la class close queryselector
+const bground = document.querySelector(".bground");//on va cherche la class bground queryselector
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +22,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+//pour fermre le formulaire
+close.addEventListener("click", () => { //on met un evenement au click sur close pour pouvoir fermer le formulaire
+  bground.style.display = "none"; //avec display none on ferme le formulaire 
+});
