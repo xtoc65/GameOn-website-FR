03f1797d4e6f44db2b01d46e89248db96357eb48
flora paul <102740155+xtoc65@users.noreply.github.com>
2024-02-13 20:51:54 +0100

    chexboc

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,18 +13,20 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const close = document.querySelector(".close"); //on va cherche la class close queryselector
 const bground = document.querySelector(".bground");//on va cherche la class bground queryselector
+const form = document.getElementById("form-id");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block";  
+  form.reset();
 }
 
 //pour fermre le formulaire
 close.addEventListener("click", () => { //on met un evenement au click sur close pour pouvoir fermer le formulaire
-  bground.style.display = "none"; //avec display none on ferme le formulaire 
+  bground.style.display = "none"; //avec display none on ferme le formulaire   
 });
 
 // pour validé le formulaire 
@@ -35,13 +37,22 @@ const validationFormulaire = () =>{
   let email = document.getElementById("email");
   let anniv = document.getElementById("birthdate");
   let nmbTournoi = document.getElementById("quantity");
-  const condition = document.querySelector(".checkbox2-label");
+  let nyCheckbox = document.querySelector(".checkbox-input");
+  let sfCheckbox = document.querySelector(".checkbox-input");
+  let seattleCheckbox = document.querySelector(".checkbox-input");
+  let chicagoCheckbox = document.querySelector(".checkbox-input");
+  let bostonCheckbox = document.querySelector(".checkbox-input");
+  let portlandCheckbox = document.querySelector(".checkbox-input");
+  let condition = document.querySelector(".checkbox2-label");
   const valide = document.querySelector(".btn-submit");
+  const $inscriptionOk = document.getElementById("valide-id");
   const $messagePrenom = document.getElementById("message-prenom");
   const $messageNom = document.getElementById("message-nom");
   const $messageEmail = document.getElementById("message-email");
   const $messageAnniv = document.getElementById("message-anniv");
   const $messagenmbTournoi = document.getElementById("message-nmbtournoi");
+  const $messageVille = document.getElementById("message-ville");
+  const $messageCondition = document.getElementById("message-condition")
  
   valide.addEventListener("click", (event) => {
     event.preventDefault(); //on enleve le comportement par defaut
@@ -66,8 +77,16 @@ const validationFormulaire = () =>{
       $messagenmbTournoi.style.display = "block";
       isFormValid = false;
     }
+    if (!nyCheckbox.checked || !sfCheckbox.checked || !seattleCheckbox.checked || !bostonCheckbox.checked || !chicagoCheckbox.checked || !portlandCheckbox.checked){
+      $messageVille.style.display = "block";
+      isFormValid = false;
+    }
+    if(!condition.checked){
+      $messageCondition.style.display = "block";
+      isFormValid = false;
+    }
     if(isFormValid){
-      //TODO: cacher tout les span avec la classe hasError allez regarder la documentation
+      //cache tout les span avec la classe hasError
       // Sélectionnez tous les éléments avec la classe spécifiée
         const hasError = document.getElementsByClassName('has-error');
 
@@ -75,7 +94,7 @@ const validationFormulaire = () =>{
         for (i = 0; i < hasError.length; i++) {
             hasError[i].style.display = 'none';
         }
-        alert("vous avez validé le formulaire")
+        $inscriptionOk.style.display = "block";
     }
 
   });
